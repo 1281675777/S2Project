dc3d197410b931d5f5fab1d90698ede9c33803a1
2262802623 <2262802623@qq.com>
2020-01-05 10:23:03 +0800

    ddd

---
+++ s_investigate.java
@@ -0,0 +1,75 @@
+package sInvestigateEntity;
+
+public class s_investigate {
+	private int chk_id;
+	public int getChk_id() {
+		return chk_id;
+	}
+	public void setChk_id(int chk_id) {
+		this.chk_id = chk_id;
+	}
+	public String getChk_option() {
+		return chk_option;
+	}
+	public void setChk_option(String chk_option) {
+		this.chk_option = chk_option;
+	}
+	public String getChk_name() {
+		return chk_name;
+	}
+	public void setChk_name(String chk_name) {
+		this.chk_name = chk_name;
+	}
+	public String getChk_date() {
+		return chk_date;
+	}
+	public void setChk_date(String chk_date) {
+		this.chk_date = chk_date;
+	}
+	public String getViolat_flag() {
+		return violat_flag;
+	}
+	public void setViolat_flag(String violat_flag) {
+		this.violat_flag = violat_flag;
+	}
+	public String getViolate_cesc() {
+		return violate_cesc;
+	}
+	public void setViolate_cesc(String violate_cesc) {
+		this.violate_cesc = violate_cesc;
+	}
+	public String getChk_remark() {
+		return chk_remark;
+	}
+	public void setChk_remark(String chk_remark) {
+		this.chk_remark = chk_remark;
+	}
+	public String getApp_no() {
+		return app_no;
+	}
+	public void setApp_no(String app_no) {
+		this.app_no = app_no;
+	}
+	public String getPrio_code() {
+		return prio_code;
+	}
+	public void setPrio_code(String prio_code) {
+		this.prio_code = prio_code;
+	}
+	public String getKeycons_file() {
+		return keycons_file;
+	}
+	public void setKeycons_file(String keycons_file) {
+		this.keycons_file = keycons_file;
+	}
+	private String chk_option;
+	private String chk_name;
+	private String chk_date;
+	private String violat_flag;
+	private String violate_cesc;
+	private String chk_remark;
+	private String app_no;
+	private String prio_code;
+	private String keycons_file;
+	
+}
